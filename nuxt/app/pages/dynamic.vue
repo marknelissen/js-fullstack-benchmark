07040f2d42c2cc8af914ceee1ce95919ee2eb588
Mark Nelissen <mark.nelissen@gemsotec.com>
2025-11-24 14:12:59 +0100

    keep server state for date

--- nuxt/app/pages/dynamic.vue
+++ nuxt/app/pages/dynamic.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 const { data } = await useFetch("/api/id");
+const now = useState("now", () => new Date().toString());
 </script>
 
 <template>
   <h1>Hello Nuxt 3!</h1>
-  <p>{{ new Date().toString() }} - {{ data }}</p>
+  <p>{{ now }} - {{ data }}</p>
 </template>
