25bf9c6832211e6a648ae378f30c2f1f686a808a
Mark Nelissen <mark.nelissen@gemsotec.com>
2026-03-22 14:10:01 +0100

    start migration to v2 alfa of solid start

---
+++ solidstart-solidrouter/vite.config.ts
@@ -0,0 +1,9 @@
+import { solidStart } from "@solidjs/start/config";
+import { nitroV2Plugin } from "@solidjs/vite-plugin-nitro-2";
+import { defineConfig } from "vite";
+
+export default defineConfig(() => {
+  return {
+    plugins: [solidStart({}), nitroV2Plugin()],
+  };
+});
